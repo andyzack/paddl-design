dae36adb8e19346ddadc1a6b1b48cf56096219b3
Andrew Zacharias <andyzack@gmail.com>
2020-01-30 06:20:53 +0000

    paddl design: mobile check

--- src/Activity.js
+++ src/Activity.js
@@ -66,9 +66,10 @@ class Activity extends Component {
     return (
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-blue light-blue san-serif center tc">
         <div className="w-100 center">
+          {/* ACTIVITY HEADING SECTION */}
           <section className="bg-gradient-blue-title">
             <div className="flex justify-center items-center pt5">
-              <span className="absolute right-2 top-2 light-blue f4 pointer grow arial" 
+              <span className="absolute right-2 top-2 light-blue f4 pointer grow" 
                 onClick={e => {
                   this.onClose(e);
                 }}
@@ -77,12 +78,12 @@ class Activity extends Component {
               <div className="pa0 tl nb4 mt2">
                 <img
                   src={this.props.data.categoryicon}
-                  className="dib w4 h4"
+                  className="dib w4 icon-category"
                   alt="{this.props.data.title}"
                 />
               </div>
-              <div className="pa3 tl nb4 mt2">
-                <h1 className="ma0 f3 light-blue">{this.props.data.title}</h1>
+              <div className="pa3 tl nb3 mt0">
+                <h1 className="ma0 pb2 f3-l f4-m f4-s fw6 light-blue">{this.props.data.title}</h1>
                 <span>
                   <label className="light-purple">{this.props.data.category} | </label> {this.props.data.subtitle}
                 </span>
@@ -90,34 +91,38 @@ class Activity extends Component {
             </div>
           </section>
 
+          {/* ACTIVITY BODY SECTION */}
           <section className="bg-gradient-blue-content pt5">
             <div className="mw8 w-100 center">
-              <h2 className="f3 pb5">Activity updates</h2>
-                <div className="dt center w5">
-                  <div className="dtc w-50 nowrap tl f5">
-                    <span className="f4 icon-clock pl2">{this.props.data.totaldays}</span> days
-                  </div>
-                  <div className="dtc w-50 nowrap tr f4">
-                  <span className="light-purple f3">{this.deltadayssign()}{this.props.data.deltadays}</span> days
-                  </div>
+              <h2 className="f3-l f4-m f4-s fw6 pb5">Activity updates</h2>
+              <div className="dt center w5">
+                <div className="dtc w-50 nowrap tl f5">
+                  <span className="f4 icon-clock pl2">{this.props.data.totaldays}</span> days
                 </div>
-                <div className="dt center w5">
-                  <div className="dtc w-50 nowrap tl f5">
-                    <span className="f4 icon-arrow-up pl2">{this.props.data.totalxp}</span> XP
-                  </div>
-                  <div className="dtc w-50 nowrap tr f4">
-                    <span className="light-purple f3">{this.deltaxpsign()}{this.props.data.deltaxp}</span> XP
-                  </div>
+                <div className="dtc w-50 nowrap tr f4">
+                <span className="light-purple f3">{this.deltadayssign()}{this.props.data.deltadays}</span> days
+                </div>
               </div>
+              <div className="dt center w5">
+                <div className="dtc w-50 nowrap tl f5">
+                  <span className="f4 icon-arrow-up pl2">{this.props.data.totalxp}</span> XP
+                </div>
+                <div className="dtc w-50 nowrap tr f4">
+                  <span className="light-purple f3">{this.deltaxpsign()}{this.props.data.deltaxp}</span> XP
+                </div>
+              </div>
+
               <div className="h2 mt4 mb0 f5 light-purple">{this.renderSubComp()}</div>
               <button className="f4 no-underline grow v-mid white bg-transparent ba b--light-blue br3 ph4 pv3 mb3 mt1" onClick={this.handleSubmit.bind(this, 'openactivity')}>
                 Continue
               </button>
+              
               <div className="mt3 az-progress-bar">
                 <span className="b1 br-100 ma2 pa1 bg-light-blue dib"></span>
                 <span className="b1 br-100 ma2 pa1 bg-light-purple dib"></span>
                 <span className="b1 br-100 ma2 pa1 bg-light-blue dib"></span>
               </div>
+
             </div>
           </section>
         </div>
--- src/App.js
+++ src/App.js
@@ -36,20 +36,20 @@ class App extends Component {
     return (
       <div className="page vh-100 bg-gradient-blue dt w-100 bg-right no-repeat cover">
         <header className="sans-serif">
-          <div className="cover bg-left bg-center-l bg-image">
+          <div className="cover bg-left bg-center bg-image">
             <div className="bg-black-80 pb5 pb6-m pb7-l">
               <nav className="dt w-100 mw8 center">
                 <div className="dtc w2 v-mid pa3">
                   <a href="/" className="dib w3 h3 pa1 grow-large">
                     <img
                       src={logo}
-                      className="logo h3"
+                      className="logo"
                       alt="AAZ"
                     />
                   </a>
                 </div>
               </nav>
-              <div className="tc-l mt4 mt5-m mt6-l ph3">
+              <div className="tc mt4 mt5-m mt6-l ph3">
                 <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Student's Portal</h1>
                 <h2 className="fw1 f3 white-80 mt3 mb4">Connect to real employment experiences</h2>
                 <button className="f4 no-underline grow v-mid bg-dark-gray white ba b--light-blue br3 ph4 pv3 mb3" 
