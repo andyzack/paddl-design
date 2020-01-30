0748a13051c103715c156f6378799e2039cefb04
Andrew Zacharias <andyzack@gmail.com>
2020-01-30 05:22:11 +0000

    paddl design: initial commit

---
+++ src/Activity.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Activity extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      render:'',
+      totaldays: 287,
+      deltadays: +26,
+      totalxp: 3244,
+      deltaxp: -420,
+      activityUpdateStatus: '',
+      title: "Bachelor of Communications (Media)",
+      subtitle: "Bachelor Degree",
+      category: "Education",
+      categoryicon: "[IMG]",
+    };
+  }
+
+  static defaultProps = {
+    id: "",
+    modalClass: "",
+    modalSize: "md"
+  };
+
+  handleSubmit(obj, e) {
+    this.setState({render:obj});
+  }
+
+  renderSubComp() {
+    switch(this.state.render){
+      case 'openactivity':
+        return (
+          <div>Progress Updated!</div>
+        )
+      default: return ''
+    }
+  }
+
+  removeSubComp() {
+    this.setState({ render: false });
+  }
+
+  onClose = e => {
+    this.props.onClose && this.props.onClose(e);
+    this.setState({ render: false });
+  };
+
+  deltadayssign() {
+    if (this.props.data.deltadays > 0) {
+      return '+'
+    }
+  }
+
+  deltaxpsign() {
+    if (this.props.data.deltaxp > 0) {
+      return '+'
+    }
+  }
+
+  render() {
+    if(!this.props.show){
+      return null;
+    }
+    return (
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-blue light-blue san-serif center tc">
+        <div className="w-100 center">
+          <section className="bg-gradient-blue-title">
+            <div className="flex justify-center items-center pt5">
+              <span className="absolute right-2 top-2 light-blue f4 pointer grow arial" 
+                onClick={e => {
+                  this.onClose(e);
+                }}
+              >X
+              </span>
+              <div className="pa0 tl nb4 mt2">
+                <img
+                  src={this.props.data.categoryicon}
+                  className="dib w4 h4"
+                  alt="{this.props.data.title}"
+                />
+              </div>
+              <div className="pa3 tl nb4 mt2">
+                <h1 className="ma0 f3 light-blue">{this.props.data.title}</h1>
+                <span>
+                  <label className="light-purple">{this.props.data.category} | </label> {this.props.data.subtitle}
+                </span>
+              </div>
+            </div>
+          </section>
+
+          <section className="bg-gradient-blue-content pt5">
+            <div className="mw8 w-100 center">
+              <h2 className="f3 pb5">Activity updates</h2>
+                <div className="dt center w5">
+                  <div className="dtc w-50 nowrap tl f5">
+                    <span className="f4 icon-clock pl2">{this.props.data.totaldays}</span> days
+                  </div>
+                  <div className="dtc w-50 nowrap tr f4">
+                  <span className="light-purple f3">{this.deltadayssign()}{this.props.data.deltadays}</span> days
+                  </div>
+                </div>
+                <div className="dt center w5">
+                  <div className="dtc w-50 nowrap tl f5">
+                    <span className="f4 icon-arrow-up pl2">{this.props.data.totalxp}</span> XP
+                  </div>
+                  <div className="dtc w-50 nowrap tr f4">
+                    <span className="light-purple f3">{this.deltaxpsign()}{this.props.data.deltaxp}</span> XP
+                  </div>
+              </div>
+              <div className="h2 mt4 mb0 f5 light-purple">{this.renderSubComp()}</div>
+              <button className="f4 no-underline grow v-mid white bg-transparent ba b--light-blue br3 ph4 pv3 mb3 mt1" onClick={this.handleSubmit.bind(this, 'openactivity')}>
+                Continue
+              </button>
+              <div className="mt3 az-progress-bar">
+                <span className="b1 br-100 ma2 pa1 bg-light-blue dib"></span>
+                <span className="b1 br-100 ma2 pa1 bg-light-purple dib"></span>
+                <span className="b1 br-100 ma2 pa1 bg-light-blue dib"></span>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    )
+  }
+}
+
+Activity.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired,
+  categoryicon: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+  totaldays: PropTypes.number.isRequired,
+  totalxp: PropTypes.string,
+  deltadays: PropTypes.number.isRequired,
+  deltaxp: PropTypes.string,
+}
+
+export default Activity;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Activity from './Activity.js';
+import './assets/sass/index.scss';
+import logo from './assets/images/logo.png';
+import iconcategory from './assets/images/icon-category.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      render:'',
+      show: false,
+      totaldays: 287,
+      deltadays: +26,
+      totalxp: 3244,
+      deltaxp: -420,
+      activityUpdateStatus: '',
+      title: "Bachelor of Communications (Media)",
+      subtitle: "Bachelor Degree",
+      category: "Education",
+      categoryicon: iconcategory,
+    };
+  }
+
+  handleSubmit(obj, e) {
+    this.setState({render:obj});        
+  }
+
+  showModal = e => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+  
+  render() {
+    return (
+      <div className="page vh-100 bg-gradient-blue dt w-100 bg-right no-repeat cover">
+        <header className="sans-serif">
+          <div className="cover bg-left bg-center-l bg-image">
+            <div className="bg-black-80 pb5 pb6-m pb7-l">
+              <nav className="dt w-100 mw8 center">
+                <div className="dtc w2 v-mid pa3">
+                  <a href="/" className="dib w3 h3 pa1 grow-large">
+                    <img
+                      src={logo}
+                      className="logo h3"
+                      alt="AAZ"
+                    />
+                  </a>
+                </div>
+              </nav>
+              <div className="tc-l mt4 mt5-m mt6-l ph3">
+                <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Student's Portal</h1>
+                <h2 className="fw1 f3 white-80 mt3 mb4">Connect to real employment experiences</h2>
+                <button className="f4 no-underline grow v-mid bg-dark-gray white ba b--light-blue br3 ph4 pv3 mb3" 
+                  onClick={
+                    e => {
+                      this.showModal();
+                    }
+                  }
+                >
+                  Activity updates
+                </button>
+
+                <Activity data={this.state} onClose={this.showModal} show={this.state.show} />
+
+              </div>
+            </div>
+          </div>
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
