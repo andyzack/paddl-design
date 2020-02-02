4eca4d06a3677781d4ee46757d086aa257f8e992
Andrew Zacharias <andyzack@gmail.com>
2020-02-02 10:37:40 +0000

    Updates

---
+++ precache-manifest.c46640125de8c48f32642919ddb6fdbd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fd29968ccb3960757f46adba272888",
+    "url": "/paddl-design/index.html"
+  },
+  {
+    "revision": "f7ede61144b17f227769",
+    "url": "/paddl-design/static/css/main.04c11bd0.chunk.css"
+  },
+  {
+    "revision": "48dab1b2f3f7f188bcb0",
+    "url": "/paddl-design/static/js/2.2743bfcc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/paddl-design/static/js/2.2743bfcc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7ede61144b17f227769",
+    "url": "/paddl-design/static/js/main.cb098b37.chunk.js"
+  },
+  {
+    "revision": "1f370669e11ed58d80e8",
+    "url": "/paddl-design/static/js/runtime-main.81323fc8.js"
+  },
+  {
+    "revision": "a03c3623bb349c5591a7d37a31d0e31f",
+    "url": "/paddl-design/static/media/icon-arrow-up.a03c3623.svg"
+  },
+  {
+    "revision": "c6acdec027560c07692fc99e4384bcd0",
+    "url": "/paddl-design/static/media/icon-category.c6acdec0.png"
+  },
+  {
+    "revision": "9f3826baecc09add101938928da0de53",
+    "url": "/paddl-design/static/media/icon-clock.9f3826ba.svg"
+  },
+  {
+    "revision": "281e5f7b26fdb141709340d4c3fed940",
+    "url": "/paddl-design/static/media/logo.281e5f7b.png"
+  }
+]);
